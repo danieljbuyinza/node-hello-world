19f315cd1e462fd4e44d7375de67455ffeffbf92
danieljbuyinza <danieljbuyinza@gmail.com>
2019-11-05 18:40:14 +0300

    Established connection with the server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+//console.log("Hello World")
+
+const express = require("express")
+const app = express()
+
+app.listen(4000, function() {
+    console.log("listening on 4000")
+})
+
+app.get('/', function(req, res) {
+    res.send("Hello World")
+})
