5496ad8edf4bb6551b634b373146a13502ca93f4
danieljbuyinza <danieljbuyinza@gmail.com>
2019-11-06 10:12:35 +0300

    Added the handling for possible error pages

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const express = require("express")
 const app = express()
 
-app.listen(2000, () => {
-    console.log("listening on 2000")
+app.listen(3000, () => {
+    console.log("listening on 3000")
 })
 
 app.get('/', (req, res) => {
@@ -13,4 +13,24 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.send("This is the about us page")
+})
+
+app.post('/', (req, res) => {
+    res.send("Got a POST request")
+})
+
+app.put('/user', (req, res) => {
+    res.send("Got a PUT request at /user")
+})
+
+app.delete('/user', (req, res) => {
+    res.send("Got a DELETE request at /user")
+})
+
+app.get('/users/:name', (req, res) => {
+    res.send("Hello" + req.params.name)
+})
+
+app.get('*', (req, res) => {
+    res.send("Sorry, we didn't find that page")
 })
